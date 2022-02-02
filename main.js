b7cb65742155616ddfdb9406d06cf2c63e78e82c
unknown <tomasz.czechlewski@gmail.com>
2022-02-02 15:05:06 +0100

    html and css structure added. start with js code

---
+++ main.js
@@ -0,0 +1,4 @@
+const name = document.querySelector('.name')
+const pass1 = document.querySelector('.pass1')
+const pass2 = document.querySelector('.pass2')
+const email = document.querySelector('email')
