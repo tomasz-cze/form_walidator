1cc6292a5c24121a00b63b042045e90fb501ccbe
unknown <tomasz.czechlewski@gmail.com>
2022-02-02 16:55:42 +0100

    function of checking password added in js

--- main.js
+++ main.js
@@ -5,21 +5,58 @@ const pass2 = document.querySelector('.pass2')
 const email = document.querySelector('.email')
 //allerts paragraphs
 const alertName = document.querySelector('.alertName')
-const alertPass1 = document.querySelector('.alertName')
-const alertPass2 = document.querySelector('.alertName')
-const alertEmail = document.querySelector('.alertName')
+const alertPass1 = document.querySelector('.alertPass1')
+const alertPass2 = document.querySelector('.alertPass2')
+const alertEmail = document.querySelector('.alertEmail')
 
 // buttons
 const btnClear = document.querySelector('.clear')
 const btnSend = document.querySelector('.send')
 
-const checking = () => {
+// variables to checking password
+
+const special = /[!@#$%^&*()]/
+const numbers = /[0-9]/
+
+
+const mainChecking = () => {
+ checkingName()
+ checkingPass1()
+//  checkingPass2()
+//  checkingEmail()
+
+
+}
+const checkingName = () => {
 	const userName = name.value
+
 
 	if (userName.length >= 5) {
 		alertName.textContent = 'Nazwa użytkownika prawidłowa'
-	}
-	console.log(alertName)
+        alertName.style.color = 'green'
+	} else {
+        alertName.textContent = 'Za krótka nazwa. Minimum 5 znaków!'
+        alertName.style.color = 'red'
+    }
+	
+}
+const checkingPass1 = () => {
+    const passsword1 = pass1.value
+
+    if (passsword1.length >= 8  && passsword1.match(special) && passsword1.match(numbers)) {
+        alertPass1.textContent = "Hasło prawidłowe"
+        alertPass1.style.color = 'green'
+    } else if (passsword1.length >= 8 && passsword1.match(special) ) {
+        alertPass1.textContent = "W haśle brakuje cyfry"
+        alertPass1.style.color = 'red'
+    } else if (passsword1.length >= 8  && passsword1.match(numbers)) {
+        alertPass1.textContent = "W haśle brakuje znaku specjalnego."
+        alertPass1.style.color = 'red'
+    } else if (passsword1.length >= 8) {
+        alertPass1.textContent = "W haśle brakuje znaku specjalnego i cyfry."
+        alertPass1.style.color = 'red'
+    }
+    console.log(passsword1)
 }
 
 const clearing = () => {
@@ -34,4 +71,4 @@ const clearing = () => {
 }
 
 btnClear.addEventListener('click', clearing)
-btnSend.addEventListener('click', checking)
+btnSend.addEventListener('click', mainChecking)
